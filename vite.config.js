874ceaf128aa2abe36b26d2d4f84e6ca7bbf1cce
Chiranjeevi Bingimalla <chiranjeevi.bingimalla@sky.uk>
2026-03-15 13:43:18 +0530

    Fix github pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/subramanyeswara-paper-plates/',
+  base: '/subramanyeswara-paper-plates-landing-page/',
 })
